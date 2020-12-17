5943a02740dc668391047b69dbb71bd3e2e56676
nandini-garg4 <73828366+nandini-garg4@users.noreply.github.com>
2020-12-17 11:58:50 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,34 @@
+class Paper {
+
+    constructor (x,y,radius){
+
+        var options = {
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+
+        this.body = Bodies.circle (x,y,25,options);
+        
+        World.add(world,this.body);
+        this.radius = radius;
+
+    }
+
+    display(){
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+       
+        push();
+        
+        translate(pos.x,pos.y);
+        rotate(angle);
+        
+        
+
+        pop();
+    }
+
+}
